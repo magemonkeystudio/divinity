c441dccdd70172c4c111314029885d063ef8d3c6
Travja <the.only.t.craft@gmail.com>
2021-09-04 00:11:38 -0600

    Fix give w/ Material

--- src/main/java/su/nightexpress/quantumrpg/modules/command/MGetCmd.java
+++ src/main/java/su/nightexpress/quantumrpg/modules/command/MGetCmd.java
@@ -14,7 +14,9 @@ import su.nightexpress.quantumrpg.modules.list.itemgenerator.ItemGeneratorManage
 import su.nightexpress.quantumrpg.modules.list.itemgenerator.ItemGeneratorManager.GeneratorItem;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MGetCmd extends MCmd<QModuleDrop<?>> {
@@ -28,7 +30,7 @@ public class MGetCmd extends MCmd<QModuleDrop<?>> {
 	public String usage() {
 		return plugin.lang().Module_Cmd_Get_Usage.getMsg();
 	}
-	
+
 	@Override
 	@NotNull
 	public String description() {
@@ -52,7 +54,7 @@ public class MGetCmd extends MCmd<QModuleDrop<?>> {
 		if (i == 3) {
 			return Arrays.asList("1", "10");
 		}
-		
+
 		// Support for material argument for ItemGenerator
 		if (i == 4 && this.module instanceof ItemGeneratorManager) {
 			ItemGeneratorManager itemGeneratorManager = (ItemGeneratorManager) this.module;
@@ -72,11 +74,11 @@ public class MGetCmd extends MCmd<QModuleDrop<?>> {
 		if (args.length >= 2) {
 			id = args[1];
 		}
-		
+
 		int amount = 1;
 		int lMin = -1;
 		int lMax = -1;
-		
+
 		if (args.length >= 3) {
 			if (args[2].contains(":")) {
 				String[] split = args[2].split(":");
@@ -90,17 +92,19 @@ public class MGetCmd extends MCmd<QModuleDrop<?>> {
 		if (args.length >= 4) {
 			amount = this.getNumI(sender, args[3], 1);
 		}
-		
+
 		Player p = (Player) sender;
 		ItemStack item = null;
-		
+
 		Material material = args.length >= 5 ? Material.getMaterial(args[4].toUpperCase()) : null;
 		ItemGeneratorManager itemGenerator = this.module instanceof ItemGeneratorManager ? (ItemGeneratorManager) this.module : null;
 		GeneratorItem generatorItem = itemGenerator != null ? itemGenerator.getItemById(id) : null;
-		
+
+		Map<String, Integer> addedItems = new HashMap<>();
+
 		for (int i = 0; i < amount; i++) {
 			int iLevel = Rnd.get(lMin, lMax);
-			
+
 			if (material != null && generatorItem != null) {
 				item = generatorItem.create(iLevel, -1, material);
 			}
@@ -108,13 +112,19 @@ public class MGetCmd extends MCmd<QModuleDrop<?>> {
 				item = QuantumAPI.getItemByModule(this.module, id, iLevel, -1, -1);
 			}
 			if (item == null) continue;
-			
+
 			ItemUT.addItem(p, item);
-			
+
+			String name = ItemUT.getItemName(item);
+			addedItems.put(name, addedItems.containsKey(name) ? addedItems.get(name) + 1 : 1);
+		}
+
+		for (String name : addedItems.keySet()) {
 			plugin.lang().Module_Cmd_Get_Done
-					.replace("%item%", ItemUT.getItemName(item))
-					.replace("%amount%", String.valueOf(item.getAmount()))
+					.replace("%item%", name)
+					.replace("%amount%", String.valueOf(addedItems.get(name)))
 					.send(sender);
 		}
+
 	}
 }
