13b944c5085fe8c5470f494d870064a074ba364e
Travja <the.only.t.craft@gmail.com>
2024-09-03 21:09:15 -0600

    Update to 1.21.1

---
+++ src/main/java/studio/magemonkey/divinity/nms/packets/versions/V1_21_R2.java
@@ -0,0 +1,8 @@
+package studio.magemonkey.divinity.nms.packets.versions;
+
+import org.jetbrains.annotations.NotNull;
+import studio.magemonkey.divinity.Divinity;
+
+public class V1_21_R2 extends V1_21_R1 {
+    public V1_21_R2(@NotNull Divinity plugin) {super(plugin);}
+}
