745f01d82b5e662ffe9ab64c66bd7c337fed8d81
Trav <the.only.t.craft@gmail.com>
2026-06-30 22:19:41 -0600

    feat: add 26.2 packet handling

---
+++ src/main/java/studio/magemonkey/divinity/nms/packets/versions/V26_R1.java
@@ -0,0 +1,8 @@
+package studio.magemonkey.divinity.nms.packets.versions;
+
+import org.jetbrains.annotations.NotNull;
+import studio.magemonkey.divinity.Divinity;
+
+public class V26_R1 extends V1_21_R7 {
+    public V26_R1(@NotNull Divinity plugin) {super(plugin);}
+}
