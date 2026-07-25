6011695812747735ffb3702a7fd9c13af0eba63c
MaksyKun <77341370+MaksyKun@users.noreply.github.com>
2026-07-25 08:44:53 +0200

    Update src/main/java/studio/magemonkey/divinity/config/EngineCfg.java
    Co-authored-by: Trav <the.only.t.craft@gmail.com>

--- src/main/java/studio/magemonkey/divinity/config/EngineCfg.java
+++ src/main/java/studio/magemonkey/divinity/config/EngineCfg.java
@@ -115,7 +115,7 @@ public class EngineCfg {
     public static String LORE_STYLE_FABLED_ATTRIBUTE_FORMAT;
 
     public static boolean VANILLA_ONLY_ENTITY_STATS;
-    public static boolean LEGACY_VANILLA_ITEM_STATS;
+    public static boolean VANILLA_ONLY_ITEM_STATS;
 
     public void setup() {
         this.plugin.info("Loading engine configuration...");
